1912e89b84ef6b0b795d514e57a0aee25c957dac
Ihor Horash <gorashigor@gmail.com>
2017-07-30 18:24:43 +0300

    NG routing was added

--- WeatherAppClientNG/src/app/app.module.ts
+++ WeatherAppClientNG/src/app/app.module.ts
@@ -2,16 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
 
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { QueryComponent } from './query/query.component';
+import { CitiesComponent } from './cities/cities.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QueryComponent
+    QueryComponent,
+    CitiesComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/query',
+        pathMatch: 'full'
+      },
+      {
+        path: 'query',
+        component: QueryComponent
+      },
+      {
+        path: 'cities',
+        component: CitiesComponent
+      },
+      {
+        path: 'history',
+        component: HistoryComponent
+      }
+    ]),
     MdToolbarModule,
   ],
   providers: [],
